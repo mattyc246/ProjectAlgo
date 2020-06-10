fb1340e299c3b56aeb640de31b57fca1a0d7a603
Matthew Cross <mattyc246@gmail.com>
2020-06-10 17:11:51 +0800

    copy invite url added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import DashboardLayout from '../containers/DashboardLayout'
+import UserInvite from "../components/UserInvite"
 
 const Dashboard = () => {
   return (
     <DashboardLayout page="dashboard">
-      
+      <UserInvite />
     </DashboardLayout>
   )
 }
